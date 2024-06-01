7cdf073ec97befb09bddf8a59a87b1b9abd36e93
Andy <134444257+Just-Andy404@users.noreply.github.com>
2024-06-01 17:18:48 +0300

    Update 8.04.2024.cpp

--- 8.04.2024.cpp
+++ 8.04.2024.cpp
@@ -35,18 +35,20 @@ int main() {
     while (true) {
         cout << "1) add item;" << endl;
         cout << "2) print items;" << endl;
-        cout << "3) approximate volume output;" << endl;
-        cout << "4) Determination of water surface area;" << endl;
-        cout << "5) check whether water bodies belong to the same type;" << endl;
-        cout << "6) compare the water surface area of water bodies of the same type;" << endl;
+        cout << "3) remove item;" << endl;
+        cout << "4) approximate volume output;" << endl;
+        cout << "5) Determination of water surface area;" << endl;
+        cout << "6) check whether water bodies belong to the same type;" << endl;
+        cout << "7) compare the water surface area of water bodies of the same type;" << endl;
         cout << "0) exit;" << endl;
         cin >> choice;
+        cout << "================================\n";
         switch (choice)
         {
         case 0:
-            exit(0);
+            cout << "Goodbye !";
+            return 0;
         case 1:
-            howMany_Items++;
             if (!isInitializer) 
             {
                 list = new Reservoir[howMany_Items];
@@ -56,75 +58,79 @@ int main() {
                 list[i].input();
                 cout << "================================\n";
             }
-                //list->add(list, howMany_Items);
+            howMany_Items++;
             isInitializer = true;
-            /*size = list->getLength();
-            list->input();
-            cout << "================================\n";
-            if (isInitializer)
-            {
-                list->add(list, howMany_Items);
-            }
-            isInitializer = true;*/
             break;
         case 2:
-            for (int i = 1; i <= howMany_Items; i++)
+            for (int i = 0; i < howMany_Items; i++)
             {
+                cout << "id: " << i << endl;
                 list[i].output();
                 cout << "================================\n";
             }
             break;
         case 3:
-            for (int i = 0; i < howMany_Items; i++)
-            {
-                cout << "approximate volume output is " << list[i].Determining_the_approximate_volume();
+            int deleteItem;
+            cout << "Which item do you want delete ?\n";
+            cin >> deleteItem;
+            if (deleteItem < 0 || deleteItem >= howMany_Items) {
+                cout << "Неверный индекс" << endl;
+                break;
             }
+            cout << "================================\n";
+            list->remove(list, deleteItem, howMany_Items);
+            howMany_Items--;
             break;
         case 4:
             for (int i = 0; i < howMany_Items; i++)
             {
-                cout << "Determination of water surface area is " << list[i].water_surface_area();
+                cout << "approximate volume output from " << list[i].getName() << " is " << list[i].Determining_the_approximate_volume() << endl;
             }
+            cout << "================================\n";
             break;
         case 5:
             for (int i = 0; i < howMany_Items; i++)
             {
-                cout << "Determination of water surface area is " << list[i].water_surface_area();
+                cout << "Determination of water surface area from " << list[i].getName() << " is " << list[i].water_surface_area() << endl;
             }
+            cout << "================================\n";
             break;
         case 6:
-            for (int i = 0; i < howMany_Items; i++)
+            for (int i = 0; i+1 < howMany_Items; i++)
             {
                 if (list[i].check_if_the_type_of_water(list[i + 1]))
                 {
                     cout << list[i].getName() << " and " <<
-                        list[i + 1].getName() << list[i].getType();
+                        list[i + 1].getName() << " is " << list[i].getType() << endl;
                 }
                 else if (!list[i].check_if_the_type_of_water(list[i + 1]))
                 {
                     cout << list[i].getName() << " and " <<
                         list[i + 1].getName() <<
-                        " it's not the same type.";
+                        " it's not the same type." << endl;
                 }
             }
+            cout << "================================\n";
             break;
         case 7:
-            for (int i = 0; i < howMany_Items; i++)
+            for (int i = 0; i+1 < howMany_Items; i++)
             {
                 if (list[i].check_if_the_type_of_water(list[i + 1]))
                 {
-                    cout << list[i].ater_bodies_belong_to_the_same_type(list[i + 1]);
+                    cout << list[i].ater_bodies_belong_to_the_same_type(list[i + 1]) << endl;
                 }
                 else if (!list[i].check_if_the_type_of_water(list[i + 1]))
                 {
                     cout << list[i].getName() << " and " <<
                         list[i + 1].getName() <<
-                        " it's not the same type.";
+                        " it's not the same type." << endl;
                 }
             }
+            cout << "================================\n";
             break;
         default:
             cout << "Invalid choice, try again." << endl;
+            cout << "================================\n";
         }
     }
 }
